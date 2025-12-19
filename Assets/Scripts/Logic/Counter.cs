a88e28269772f167d3f88a729a00f55e91612aee
Toastid Waffel <toastidwaffel@gmail.com>
2025-12-19 13:59:41 -0500

    Adding Stuff to Timer - Rearranging

--- Assets/Scripts/Logic/Counter.cs
+++ Assets/Scripts/Logic/Counter.cs
@@ -1,15 +1,18 @@
 using UnityEngine;
 
 public class Counter : Triggerable
-{
-    // Activates things only after the right amount of effects have triggered it.
+{   // A trigger that only activates the objects assigned to it when it has been activated a certain number of times.
     int currentCount = 0;
     bool flipped = false;
 
+    [Header("Settings")]
     public int activateAt = 1;
-    [Tooltip("Sets the current count to 0 once activated.")] public bool resets = false;
-    [Tooltip("Count can go down by one with a deactivation call.")] public bool countsDeactivations = true;
-    [Tooltip("Only triggers once.")]public bool singleActivation = false;
+    [Tooltip("Sets the current count to 0 once activated.")] 
+    public bool resets = false;
+    [Tooltip("Count can go down by one with a deactivation call.")] 
+    public bool countsDeactivations = true;
+    [Tooltip("Only triggers once.")]
+    public bool singleActivation = false;
 
     public override void Activate()
     {
