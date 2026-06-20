fb1c198e5ee385a11417fc1466582bc70e33a248
Toastid Waffel <toastidwaffel@gmail.com>
2026-06-20 11:51:35 -0400

    Updated Chest Model

---
+++ Assets/Scripts/Logic/Triggers/ChestController.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+
+public class ChestController : GameTrigger
+{
+    public new string _ = "-- GameTrigger --\n" +
+    "Allows a chest to auto-open when the player is present or be locked to color-coded keys.";
+    [Header("Chest Settings")]
+    [Tooltip("")] public bool lockedWithKey = false;
+    public ColorManager.StandardColor keyColor;
+    [Header("Components")]
+    public GameObject lockIcon;
+    public bool opened = false;
+
+    // Internal Logic
+    bool stillLocked = false;
+
+    private void Start()
+    {
+        if (lockedWithKey)
+        {
+            stillLocked = true;
+            lockIcon.SetActive(true);
+            Material[] mat = new Material[2] { ColorManager.current.holoBlack, ColorManager.current.standardHolos[(int)keyColor] };
+            foreach (Renderer rend in lockIcon.GetComponentsInChildren<Renderer>())
+            {
+                rend.materials = mat;
+            }
+        }
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && !opened)
+        {
+            if (lockedWithKey && stillLocked)
+            {
+                if (ObjectivesManager.CheckKey(keyColor))
+                {
+                    HUD.DisplayNotice("Chest Unlocked");
+                    lockIcon.SetActive(false);
+                    stillLocked = false;
+                    opened = true;
+                    ActivateItems();
+                }
+                else
+                {
+                    HUD.DisplayNotice(keyColor.ToString() + " key needed.");
+                }
+            }
+            else
+            {
+                opened = true;
+                ActivateItems();
+            }
+        }
+    }
+}
