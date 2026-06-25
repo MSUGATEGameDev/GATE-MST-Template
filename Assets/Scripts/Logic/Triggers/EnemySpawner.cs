b520029e92bd546140c7d783b51f0e4e97f60bb7
Toastid Waffel <toastidwaffel@gmail.com>
2026-06-24 22:29:49 -0400

    Added Enemy Spwaners

--- Assets/Scripts/Logic/Triggers/EnemySpawner.cs
+++ Assets/Scripts/Logic/Triggers/EnemySpawner.cs
@@ -9,14 +9,30 @@ public class EnemySpawner : GameAction
     [Header("Settings")]
     [Tooltip("-1 is Infinite")]public int enemyCount = -1;
     public Enemy enemyPrefab;
-
+    Transform placer;
+    private void Awake()
+    {
+        placer = transform.GetChild(0);        
+    }
     public override void Activate()
     {
-
+        if(enemyCount > 0)
+        {
+            enemyCount--;
+            SpawnEnemy();
+        }
+        else if (enemyCount == -1)
+        {
+            SpawnEnemy();
+        }
     }
-
+    void SpawnEnemy()
+    {
+        placer.localPosition = new Vector3(Random.Range(0, 1), .824561f, Random.Range(0, 1));
+        Instantiate(enemyPrefab,placer.position,Quaternion.Euler(0,Random.Range(0,360),0));
+    }
     public override void Deactivate()
     {
-
+        // Does nothing.
     }
 }
